ffdd363395e4914609da796b7164f335767403de
Lord-Y <Lord-Y@users.noreply.github.com>
2026-04-13 23:04:34 +0200

    tests(semantics): Add tests for UntypedNilType and FuncType types

--- semantics/builtins_test.go
+++ semantics/builtins_test.go
@@ -49,6 +49,8 @@ func TestSemantics_builtins(t *testing.T) {
 			{tp: &Enum{Name: "Color", Variants: []string{"Red", "Blue", "Green"}}, expected: "enum"},
 			{tp: &SumType{Name: "Shape", Variants: []SumVariant{{Name: "Circle", Field: []Param{{Name: "radius", Type: TFloat64}}}}}, expected: "sum"},
 			{tp: &InvalidType{}, expected: "<invalid>"},
+			{tp: &UntypedNilType{}, expected: "nil"},
+			{tp: &FuncType{}, expected: "funcType"},
 		}
 
 		for _, tc := range tests {
